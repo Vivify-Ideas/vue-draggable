c357569d2fafe45f19cafe40ba28a8a890068d5a
Gabriel A. Hayes <gabriel.speed.bullock@gmail.com>
2019-01-24 16:02:32 -0600

    Add IE polyfills.

---
+++ src/polyfills/index.js
@@ -0,0 +1,39 @@
+/*
+    IE polyfills
+    closest: https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+    remove: https://developer.mozilla.org/en-US/docs/Web/API/ChildNode/remove
+*/
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector ||
+        Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function (s) {
+        var el = this;
+
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1);
+        return null;
+    };
+}
+// from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+(function (arr) {
+    arr.forEach(function (item) {
+        if (item.hasOwnProperty('remove')) {
+            return;
+        }
+        Object.defineProperty(item, 'remove', {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function remove() {
+                if (this.parentNode !== null)
+                    this.parentNode.removeChild(this);
+            }
+        });
+    });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
