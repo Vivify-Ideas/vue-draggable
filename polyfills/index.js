0f3b02c4bea2a9965c49a92f64115c323afb3632
Gabriel Hayes <gabriel.speed.bullock@gmail.com>
2019-05-08 11:43:32 -0500

    Custom Event Polyfill

--- polyfills/index.js
+++ polyfills/index.js
@@ -37,3 +37,20 @@ if (!Element.prototype.closest) {
     });
 })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
 
+// Custom Events
+// IE Polyfills: https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
+(function () {
+
+  if ( typeof window.CustomEvent === "function" ) return false;
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: null };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+})();
