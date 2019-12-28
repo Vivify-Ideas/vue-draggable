273a15dbaeac917b9aa4ea4b9e9b4dfeeaa81be4
m136278 <Pamalla.Jyothiswaroop@mayo.edu>
2019-12-27 19:45:20 -0600

    deselect items when clicked on already selected item without modifier.

--- src/core/listeners/handlers/mouseup.handler.js
+++ src/core/listeners/handlers/mouseup.handler.js
@@ -1,5 +1,5 @@
 import {
-  addSelection, removeSelection, hasModifier
+  addSelection, removeSelection, hasModifier, clearSelections
 } from './../../helpers';
 
 const isItemInSelectionArea = (item, element, lastItem) => {
@@ -19,35 +19,40 @@ export const mouseupHandler = function (e) {
   let elem = e.target.closest(this.defaultOptions.draggableSelector);
 
   // if the element is a draggable item
-  // and the multipler selection modifier is pressed
-  if (elem && elem.getAttribute('draggable') && hasModifier(e)) {
+  if (elem && elem.getAttribute('draggable')) {
     // if shift key is pressed select multiple items
-    if (this.selections.items.length && e.shiftKey) {
-      // last selected item
-      const lastItem = this.selections.items.slice(-1).pop();
+    if (hasModifier(e)) {
+      if (this.selections.items.length && e.shiftKey) {
+        // last selected item
+        const lastItem = this.selections.items.slice(-1).pop();
 
-      if (this.items && this.items.length > 0) {
-        for (let i = 0; i < this.items.length; i++) {
-          const item = this.items[i];
+        if (this.items && this.items.length > 0) {
+          for (let i = 0; i < this.items.length; i++) {
+            const item = this.items[i];
 
-          const shouldSelectItem = isItemInSelectionArea(item, elem, lastItem);
+            const shouldSelectItem = isItemInSelectionArea(item, elem, lastItem);
 
-          shouldSelectItem && addSelection.bind(this)(item);
-        } // if the item's grabbed state is currently true
-      }
+            shouldSelectItem && addSelection.bind(this)(item);
+          } // if the item's grabbed state is currently true
+        }
 
-    } else if (elem.getAttribute('aria-grabbed') === 'true') {
-      // unselect this item
-      removeSelection.bind(this)(elem);
+      } else if (elem.getAttribute('aria-grabbed') === 'true') {
+        // unselect this item
+        removeSelection.bind(this)(elem);
 
-      // if that was the only selected item
-      // then reset the owner container reference
-      if (!this.selections.items.length) {
-        this.selections.owner = null;
+        // if that was the only selected item
+        // then reset the owner container reference
+        if (!this.selections.items.length) {
+          this.selections.owner = null;
+        }
+      } else {
+        // else [if the item's grabbed state is false]
+        // add this additional selection
+        addSelection.bind(this)(elem);
       }
     } else {
-      // else [if the item's grabbed state is false]
-      // add this additional selection
+      // if no modifier, clear all selections and add current item.
+      clearSelections.bind(this)();
       addSelection.bind(this)(elem);
     }
   }
